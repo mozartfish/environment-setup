8d50da2bdde4c5be552f6711b16d0d9a4891606a
Pranav Rajan <rajan.pranav320@gmail.com>
2025-10-05 09:29:58 +0200

    setup gpus

--- ml-environment-setup/notebook1.py
+++ ml-environment-setup/notebook1.py
@@ -6,8 +6,41 @@ app = marimo.App(width="medium")
 
 @app.cell
 def _():
+    import marimo as mo
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import torch
+    return mo, torch
 
 
+@app.cell
+def _(mo):
+    mo.md("""## Setup GPU""")
+    return
+
+
+@app.cell
+def _(torch):
+    if __name__ == "__main__":
+        if torch.cuda.is_available():
+            device = torch.device("cuda")
+        elif torch.backends.mps.is_available():
+            device = torch.device("mps")
+        else:
+            device = torch.device("cpu")
+
+        print("Device:", device)
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
     return
 
 
